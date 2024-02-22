75a0bc8c366d6e91466fba757ae43c1b34a0a935
Rakibul <mdrakibulhassan2425@gmail.com>
2024-02-22 10:05:34 +0600

    add new feture

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function getClasses (classes) {
 function makeElem (name) {
     return document.createElement(name);
 }
+// Function for discount 
+function discount (currentValue,discountValue) {
+    return (currentValue * discountValue) / 100; 
+}
 // Get the Element where i show player info 
 const playerInfo = getById("appentInfo");
 // Get total cost element 
@@ -20,6 +24,11 @@ const grandTotal = getById("grandTotal");
 // Get the budge 
 const budge = getById("budget");
 let budgeValue = 2500;
+// get left card here 
+const leftCard = getById("left");
+let leftVardValue = 6;
+// Get card value 
+const cardlist = getById("card");
 // Get all add-cart buttons 
 const buttons = getClasses("add-btn");
 for (btn of buttons) {
@@ -29,19 +38,38 @@ for (btn of buttons) {
         const numberPrice = parseInt(price);
         const category = e.target.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
         
-        if (budgeValue > 0) {
+        if (leftVardValue > 0) {
+            leftVardValue -= 1;
+            leftCard.innerText = leftVardValue;
+            if (budgeValue >= numberPrice) {
             const div = makeElem("div");
-        div.style.display = "flex";
-        div.style.justifyContent = "space-evenly";
-        const p = `<p>${name}</p>
-        <p>${numberPrice}</p>
-        <p>${category}</p>`;
-        div.innerHTML = p;
-        playerInfo.appendChild(div);
-        totalCost.innerText = totalCostNumber += numberPrice;
-        grandTotal.innerText = totalCostNumber;
+            div.style.display = "flex";
+            div.style.justifyContent = "space-evenly";
+            const p = `<p>${name}</p>
+            <p>${numberPrice}</p>
+            <p>${category}</p>`;
+            div.innerHTML = p;
+            playerInfo.appendChild(div);
+            totalCost.innerText = totalCostNumber += numberPrice;
+            grandTotal.innerText = totalCostNumber;
             budgeValue -= numberPrice;
+            cardlist.innerText = parseInt(cardlist.innerText) + 1;
+            }else {
+                alert ("Your budge is too low!")
+            }
         }
-            budge.innerText = budgeValue;
+        budge.innerText = budgeValue;
     })
 }
+const copunInput = getById('copun');
+const applyBtn = getById ("apllyBtn");
+
+function getDiscount () {
+    const copunInputValue = copunInput.value;
+    if (copunInputValue === "winner") {
+        console.log(totalCost)
+        grandTotal.innerText =  totalCostNumber - discount (totalCostNumber, 10);
+        copunInput.value = ""
+    }
+}
+applyBtn.addEventListener ("click",getDiscount)
